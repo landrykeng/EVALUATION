9270cc3c8713a562375d839c1450d71f331ba1c8
landrykeng <168802383+landrykeng@users.noreply.github.com>
2025-05-02 21:26:31 +0100

    up

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -6,6 +6,21 @@ import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 from my_fonction import *
+from Evaluation import *
 
 st.set_page_config(page_title="TABLEAU DE BORD", layout="wide")
-st.write("Tableau de bord")
+st.title("Tableau de bord pour le suivi des évaluations des enseignants")
+st.markdown(
+    """
+    <style>
+    body {
+        font-size: 20px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.dataframe(student_eval, use_container_width=True)
+st.dataframe(data_eval, use_container_width=True)
+
