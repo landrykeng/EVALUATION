b794b6dc395c1f3931a43de2d850daf86b3ea8f5
landrykeng <168802383+landrykeng@users.noreply.github.com>
2025-06-03 17:57:50 +0100

    up

--- Evaluation.py
+++ Evaluation.py
@@ -10,7 +10,7 @@ import supabase
 from supabase import create_client, Client
 
 
-st.set_page_config(page_title="FORMULAIRE EVALUATION DES ENSEIGNANTS", page_icon="📊", layout="wide")
+st.set_page_config(page_title="FORMULAIRE EVALUATION DES ENSEIGNANT", page_icon="📊", layout="wide")
 
 # Create a custom container with fancy styling for the title
 head=st.columns([4,30,4])
@@ -228,7 +228,6 @@ for _, row in data.iterrows():
 
 
 st.write("## Formulaire d'évaluation des enseignants")
-
 # Sélection de la classe
 
 classe_selectionnee = st.radio("Classe",[""] + list(nested_dict.keys()), index=0)
